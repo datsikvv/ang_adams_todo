af0beefe5f495146a914df042a552500d1e6796d
Volodymyr <31970034+datsikvv@users.noreply.github.com>
2019-10-21 18:22:37 +0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Model } from './model';
+import { VirtualTimeScheduler } from 'rxjs';
+
+
+@Component({
+  selector: 'todo-app',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  model: any = new Model();
+
+  
+  
+  getName() {
+    return this.model.user;
+    }
+
+    getTodoItems() {
+      return this.model.items;
+    }
+}
